49c7b479cf484dfa36ec6f1885a5d01c7f08bdfe
geekgarden <deific@126.com>
2022-06-06 21:50:19 +0800

    test

--- solidity/test/sample-test.js
+++ solidity/test/sample-test.js
@@ -2,12 +2,38 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("ZombieFeeding", function () {
-  it("test", async function () {
-    const ZombieFeeding = await ethers.getContractFactory("ZombieFeeding");
-    const zombie = await ZombieFeeding.deploy();
+  let zombie;
+  before(async function(done) {
+    const ZombieFeeding = await ethers.getContractFactory("ZombieOwnership");
+    zombie = await ZombieFeeding.deploy();
     await zombie.deployed();
+    console.log("deploy to:", zombie.address)
+    return zombie;
+  });
+  
+  // it("init", async function () {
+  //   const ZombieFeeding = await ethers.getContractFactory("ZombieOwnership");
+  //   const zombie = await ZombieFeeding.deploy();
+  //   await zombie.deployed();
+  //   console.log("deploy to:", zombie.address)
+  // })
 
-    await zombie.feedOnKitty(1, 1)
- 
+  it("createRandomZombie", async function () {
+    const account = await hre.ethers.getSigners()[0];
+    await zombie.createRandomZombie("test")
+    let result = await zombie.balanceOf(account);
+    console.log("balanceOf = ", result)
+    expect(result).toEqual(1);
+  });
+
+
+  it("get Zombie", async function () {
+    const account = await hre.ethers.getSigners()[0];
+    let result = await zombie.zombies(0);
+    console.log("balanceOf = ", result)
+    expect(result.name).toEqual("test");
   });
 });
+
+
+
