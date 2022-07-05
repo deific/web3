76ca75b9c4d4cc91bd16400bbde7fb5acbdbac78
geekgarden <deific@126.com>
2022-07-05 22:17:35 +0800

    add token crowdfund

---
+++ web3js/token-crowdfund/contracts/Project.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./SafeMath.sol";
+
+contract Project {
+    using SafeMath for uint256;
+
+    // Data struct
+    enum State {
+        Fundraising,
+        Expired,
+        Successful
+    }
+
+    // state variables
+}
