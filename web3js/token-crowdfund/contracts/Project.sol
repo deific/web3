98ba935393b037b5d253f386bf20f96777cd21d2
geekgarden <deific@126.com>
2022-07-07 23:17:40 +0800

    完善

--- web3js/token-crowdfund/contracts/Project.sol
+++ web3js/token-crowdfund/contracts/Project.sol
@@ -14,4 +14,16 @@ contract Project {
     }
 
     // state variables
+    address payable public creator;
+    uint public amountGoal; // required to reach at least this much,else everyone gets refud
+    uint public completeAt;
+    uint256 public currentBalance;
+    uint public raiseBy;
+    string public title;
+    string public description;
+    State public state = State.Fundraising;
+    mapping(address => uint) public contributions;
+
+    // Event that will be emitted whenever funding will be received
+    event FundingReceived(address contributor, uint amount, uint currentTotal);
 }
