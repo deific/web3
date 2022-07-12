a8d6420d9107bbb3813d7d436bb360e300d62a4a
geekgarden <deific@126.com>
2022-07-12 22:53:03 +0800

    add CrowdFunding

--- web3js/token-crowdfund/contracts/Project.sol
+++ web3js/token-crowdfund/contracts/Project.sol
@@ -55,4 +55,86 @@ contract Project {
         raiseBy = fundRaisingDeadline;
         currentBalance = 0;
     }
+
+    /** @dev Function to fund a certain project.
+     */
+    function contribute() external payable inState(State.Fundraising) {
+        require(msg.sender != creator);
+        contributions[msg.sender] = contributions[msg.sender].add(msg.value);
+        currentBalance = currentBalance.add(msg.value);
+        emit FundingReceived(msg.sender, msg.value, currentBalance);
+        checkIfFundingCompleteOrExpired();
+    }
+
+    /** @dev Function to change the project state depending on conditions.
+     */
+    function checkIfFundingCompleteOrExpired() public {
+        if (currentBalance >= amountGoal) {
+            state = State.Successful;
+            payOut();
+        } else if (now > raiseBy) {
+            state = State.Expired;
+        }
+        completeAt = now;
+    }
+
+    /** @dev Function to give the received funds to project starter.
+     */
+    function payOut() internal inState(State.Successful) returns (bool) {
+        uint256 totalRaised = currentBalance;
+        currentBalance = 0;
+
+        if (creator.send(totalRaised)) {
+            emit CreatorPaid(creator);
+            return true;
+        } else {
+            currentBalance = totalRaised;
+            state = State.Successful;
+        }
+
+        return false;
+    }
+
+    /** @dev Function to retrieve donated amount when a project expires.
+     */
+    function getRefund() public inState(State.Expired) returns (bool) {
+        require(contributions[msg.sender] > 0);
+
+        uint amountToRefund = contributions[msg.sender];
+        contributions[msg.sender] = 0;
+
+        if (!msg.sender.send(amountToRefund)) {
+            contributions[msg.sender] = amountToRefund;
+            return false;
+        } else {
+            currentBalance = currentBalance.sub(amountToRefund);
+        }
+
+        return true;
+    }
+
+    /** @dev Function to get specific information about the project.
+     * @return Returns all the project's details
+     */
+    function getDetails()
+        public
+        view
+        returns (
+            address payable projectStarter,
+            string memory projectTitle,
+            string memory projectDesc,
+            uint256 deadline,
+            State currentState,
+            uint256 currentAmount,
+            uint256 goalAmount
+        )
+    {
+        projectStarter = creator;
+        projectTitle = title;
+        projectDesc = description;
+        deadline = raiseBy;
+        currentState = state;
+        currentAmount = currentBalance;
+        goalAmount = amountGoal;
+    }
 }
